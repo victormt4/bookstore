809f5129b59e451bbed67c3f8588abb385494f61
Victor Mota <victor.vmt@outlook.com>
2023-03-10 15:58:39 -0300

    Migrando utiliários para TS

---
+++ src/utils/HttpStatus.ts
@@ -0,0 +1,24 @@
+const HttpStatus = {
+    NOT_STARTED: 0,
+    WAITING: 1,
+    ON_REQUEST_ERROR: 2,
+    requestError: function (status: number): boolean {
+        return status === this.ON_REQUEST_ERROR || (status >= 400 && status < 500);
+    },
+    serverError: function (status: number): boolean {
+        return status >= 500 && status < 600;
+    },
+    anyError: function (status) {
+        return this.requestError(status) || this.serverError(status);
+    },
+    waiting: function (status: Array<number> | number) {
+        if (Array.isArray(status)) return status.some(status => status === this.WAITING)
+        else return HttpStatus === this.WAITING;
+    },
+    done: function (status: Array<number> | number) {
+        if (Array.isArray(status)) return status.some(status => status === 200);
+        return status === 200;
+    }
+}
+
+export default HttpStatus;
--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,5 +1,5 @@
 const StringUtils = {
-    removeAccents: function (text) {
+    removeAccents: function (text: string): string {
         return text.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
     }
 }
